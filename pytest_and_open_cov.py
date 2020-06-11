cbdc9495e043989fdb92c0f623442c824e03d835
Xinyi Rong <Lizard.rar@gmail.com>
2020-06-10 21:17:15 -0700

    :construction_worker: Add coverage.py

---
+++ pytest_and_open_cov.py
@@ -0,0 +1,17 @@
+"""Run pytest and open coverage report in browser."""
+
+import webbrowser
+from pathlib import Path
+
+import pytest
+
+
+def main():
+    """Main function."""
+    pytest.main(['--cov', 'pixiv_pixie', '--cov-report', 'html'])
+    report_path = Path(__file__).parent / 'htmlcov' / 'index.html'
+    webbrowser.open(report_path.as_uri())
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,6 +1,8 @@
 """A default test."""
 
+import pixiv_pixie
+
 
 def test_hello_world():
     """A test that always passed."""
-    assert True
+    assert hasattr(pixiv_pixie, '__version__')
